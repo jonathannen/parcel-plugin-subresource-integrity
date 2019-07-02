ea282938fc7ee9ac813da4d49eab6f01d406d9f6
Jon Williams <jon@jonathannen.com>
2019-07-02 15:18:14 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# parcel-plugin-subresource-integrity
+
+Adds [Subresource Integrity](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity) attributes to any CSS and JS bundled files referenced from your [Parcel Bundler](https://parceljs.org/) entry assets.
+
+## Usage
+
+No configuration is needed. Install the package using yarn or npm and build using Parcel.
+
+By default, the SHA-384 hash algorithm will be used.
---
+++ index.js
@@ -0,0 +1,56 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+const posthtml = require("posthtml");
+
+const hashAlgorithm = "sha384";
+
+const plugin = bundler => {
+  const publicUrl = bundler.options.publicURL;
+
+  bundler.on("bundled", bundle => {
+    const bundles = Array.from(bundle.childBundles).concat([bundle]);
+
+    const promises = bundles.map(async bundle => {
+      if (!bundle.entryAsset || bundle.entryAsset.type !== "html") return;
+      const cwd = bundle.entryAsset.options.outDir;
+
+      const posthtmler = function(tree) {
+        tree.match([{ tag: "script" }, { tag: "link" }], node => {
+          let src = undefined;
+          if (node.tag === "script") {
+            src = node.attrs.src || "";
+          } else {
+            if (node.attrs.rel !== "stylesheet") return node;
+            src = node.attrs.href || "";
+          }
+          src = src.trim();
+          if (src.length === 0) return node;
+
+          // Remote publicUrl if it's in use - also detect references to external files
+          if (publicUrl) {
+            if (!src.startsWith(publicUrl)) return node;
+            src = src.substring(publicUrl.length);
+          }
+          const filename = path.join(cwd, src);
+          if (!fs.existsSync(filename)) return node;
+
+          const data = fs.readFileSync(filename, "utf8");
+          const hash = crypto.createHash(hashAlgorithm);
+          hash.update(data, "utf8");
+
+          const integrity = `${hashAlgorithm}-${hash.digest("base64")}`;
+          return { ...node, attrs: { ...node.attrs, integrity } };
+        });
+      };
+
+      const data = fs.readFileSync(bundle.name, "utf-8");
+      const result = await posthtml([posthtmler]).process(data);
+      fs.writeFileSync(bundle.name, result.html);
+    });
+
+    return Promise.all(promises);
+  });
+};
+
+module.exports = plugin;
